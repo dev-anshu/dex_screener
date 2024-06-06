5f9eb2f979a7af498a9b6bdfcab6c4bda4feb515
dev-anshu <anshu@ix.cmlab.com>
2024-06-07 02:37:15 +0530

    next-auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -16,8 +16,42 @@ export const authOptions: NextAuthOptions = {
                 password: {label: "Password", type: "password"}
             },
             async authorize(credentials: any): Promise<any> {
-                 
+                // Connect DB
+                try{
+                    // find user
+                    let user;
+
+                    if(!user) {
+                        throw new Error('No User found with this email')
+                    }
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+
+                    if(isPasswordCorrect) {
+                        return user;
+                    } else {
+                        throw new Error("Incorrect Password")
+                    }
+                
+
+                } catch(error:any) {
+                    throw new Error(error);
+                }
             }
         })
-    ]
+    ],
+    callbacks: {
+        async session({session, token}) {
+            return session;
+        },
+        async jwt({token, user}) {
+            return token;
+        }
+    },
+    pages: {
+        signIn: '/login'
+    },
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.TOKEN_SECRET
 }
